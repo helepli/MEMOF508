3ce92e4ef57c66cbde3c5dbac8459a7608584a53
helepli <helene.plisnier@ulb.ac.be>
2016-10-15 15:06:34 +0200

    Add non-maximal sequences pruning to inputProm.py

--- inputProm.py
+++ inputProm.py
@@ -1,12 +1,43 @@
 
 
-import sys  
-              
-if len(sys.argv)<2:
+import sys 
+
+maximal = False 
+
+def getMaximal(freqSeq):
+	maxLength = 0
+	for i in traces.keys():
+		if maxLength < len(freqSeq[i]):
+			maxLength = len(freqSeq[i])
+	k = maxLength
+	toBeRemoved = []
+	while k > 1:
+		for i in freqSeq.keys():
+			if len(freqSeq[i]) == k:
+				for j in freqSeq.keys():
+					if j != i:
+						if contains_sublist(freqSeq[i], freqSeq[j]):
+							toBeRemoved.append(j)
+		if len(toBeRemoved) != 0:
+			for t in toBeRemoved:
+				del freqSeq[t]
+				toBeRemoved = []					 
+		k-=1
+	return freqSeq
+	
+def contains_sublist(lst, sublst):
+    n = len(sublst)
+    return any((sublst == lst[i:i+n]) for i in range(len(lst)-n+1))
+    
+    	     
+if len(sys.argv) < 2:
 	print("This program needs a log.txt file as parameter")
 	exit()
 else:
 	filename = sys.argv[1]
+if len(sys.argv) == 3:
+	if sys.argv[2] == "-max":
+		maximal = True
 
 try:
 	log = open(filename, encoding='utf-8')
@@ -26,12 +57,17 @@ try:
 			traces[j][e] = traces[j][e].strip('<>')
 		j+=1
 		line = log.readline().strip()
-	print(traces)	
+	print(traces)
+	if maximal == True:
+		traces = getMaximal(traces)
+		print("After pruning non-maximal sequences: ")	
+		print(traces)
 except IOError:
 	print ("Error: can\'t find file or read content!")
 else:
 	print ("Opened and read the file successfully")
 	log.close()
+	
 	
 try:
 	promInput = open(filename+'.txt','wt',encoding='utf-8')
