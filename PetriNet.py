900175ee663098aa72c179f14b322127a226e436
helepli <helene.plisnier@ulb.ac.be>
2017-01-05 17:06:57 +0100

    Much better

--- PetriNet.py
+++ PetriNet.py
@@ -4,12 +4,11 @@ import numpy as np
 
 class PetriNet:
 	def __init__(self, pnFile):
-		self.transitions = [] # events, list of letters
-		self.inputPlaces = dict() # key : transition, value : number of input places
-		self.startTransitions = []
-		self.endTransitions = []
+
+		self.transitions = [] # list of Transition objects
+		self.transitionNames = []
+		self.places = [] # list of Place objects: a place  = ([input transitions], [output transitions])
 		self.placeNames = []
-		self.places = [] # list of Places: a place  = ([input transitions], [output transitions])
 		self.arcs = [] # list of tuples: one tuple = (a place, a transition) or (a transition, a place)
 		
 		check = self.parse(pnFile) # parses .dot file + construct arcs list
@@ -19,7 +18,12 @@ class PetriNet:
 		else:
 			print("Parsing went OK")
 			
-		self.getPlaces() # from the arcs, makes the places list 
+		self.setPlaces() # from the arcs, makes the places list 
+		self.setTransitions()
+		print("Arcs:")	
+		print(self.arcs)
+		self.displayPlaces()
+		
 		
 
 	def parse(self, pnFile):
@@ -44,8 +48,9 @@ class PetriNet:
 			if line == "node [fontsize=35]":
 				line = log.readline().strip()
 				while line != "}":
-					self.transitions.append(line)
-					self.inputPlaces[line] = 0
+					self.transitionNames.append(line)
+					newTransition = Transition(line)
+					self.transitions.append(newTransition)
 					line = log.readline().strip()
 			else:
 				return 'node [fontsize=35] not found, line = '+ line
@@ -57,29 +62,14 @@ class PetriNet:
 					transition = line[0].strip('{}').split(' ')
 					place = line[1]
 					self.arcs.append((transition, place))
+					
 				elif line[1] not in self.placeNames:
 					transition = line[1].strip('{}').split(' ')
 					place = line[0]
 					self.arcs.append((place, transition))
-					for t in transition:
-						self.inputPlaces[t] += 1
-				
-				if line[0] == 'start':
-					self.startTransitions.extend(line[1].strip('{}').split(' '))
-				if line[1] == 'end':
-					self.endTransitions.extend(line[0].strip('{}').split(' '))
 					
 				line = log.readline().strip()
-			print("Transitions:")	
-			print(self.transitions)
-			print("Start transitions:")
-			print(self.startTransitions)
-			print("End transitions:")
-			print(self.endTransitions)
-			print("input places:")	
-			print(self.inputPlaces)
-			print("Arcs:")	
-			print(self.arcs)
+				
 			
 		except IOError:
 			print ("Error: can\'t find file or read content!")
@@ -88,156 +78,165 @@ class PetriNet:
 			print ("Opened and read the file successfully")
 			log.close()
 			return "ok"
-			
-	def getPlaces(self):
-		for p in self.placeNames:
-			inTrans = []
-			outTrans = []
-			arcs = self.getArcsWithThisPlace(p)
-			lenArcs = len(arcs)
-			i = 0
-			temp = []
-			while lenArcs > 0:
-			
-			 	if p == arcs[i][0]:
-					outTrans = arcs[i][1]
-					lenArcs -= 1
-				if p == arcs[i][1]:
-					inTrans = arcs[i][0]
-					lenArcs -= 1
-				if len(inTrans) != 0 and len(outTrans) != 0 or p == "start" or p == "end":
-					if [p, inTrans, outTrans] not in temp:
-						newPlace = Place(p, inTrans, outTrans)
-						self.places.append(newPlace)
-						temp.append([p, inTrans, outTrans])
-				i+=1
+		
+	def getTransitionByName(self, name):
+		for t in range(len(self.transitions)):
+			if self.transitions[t].name == name:
+				return self.transitions[t]
 				
-		print("Places:")
-		self.displayPlaces(self.places)
+	def getPlaceByName(self, name):
+		for t in range(len(self.places)):
+			if self.places[t].name == name:
+				return self.places[t]
 	
-	def displayPlaces(self, setOfPlaces): # print places because a place is an object of a class Place
-		for place in setOfPlaces:
-			if place.name != "start":
-				inTrans = "("
-				for t in range(len(place.inTrans)):
-					if t == len(place.inTrans)-1:
-						inTrans += place.inTrans[t]+")"
-					else:
-						inTrans += place.inTrans[t]+", "
-			else:
-				inTrans = ""
-			if place.name != "end":
-				outTrans ="("
-				for t in range(len(place.outTrans)):
-					if t == len(place.outTrans)-1:
-						outTrans += place.outTrans[t]+")"
-					else:
-						outTrans += place.outTrans[t]+", "
-			else:
-				outTrans = ""
-					
-			print("["+inTrans+", "+place.name+", "+outTrans+"]")
-			
-			
+	
 	def getArcsWithThisPlace(self, placeName): # used by getPlaces(), returns a list of arcs with this place as input or output node
 		result = []
 		for arc in self.arcs:
 			if placeName == arc[0] or placeName == arc[1]:
 				result.append(arc)
 		return result
+		
+	def getArcsWithThisTransition(self, transName): # used by getPlaces(), returns a list of arcs with this place as input or output node
+		result = []
+		for arc in self.arcs:
+			if transName in arc[0] and arc[0] in self.transitionNames or transName in arc[1] and arc:
+				result.append(arc)
+		print(transName)
+		return result
+		
+	def setPlaces(self):
+		for i in range(len(self.placeNames)):
+			newPlace = Place(self.placeNames[i], self.getArcsWithThisPlace(self.placeNames[i]))
+			self.places.append(newPlace)
+			
+	def setTransitions(self):
+		for trans in self.transitions:
+			for place in self.places:
+				if trans.name in place.inTrans and place not in trans.outPlaces:
+					trans.setOutputPlaces(place)
+					place.inTransObj.append(trans)
+				elif trans.name in place.outTrans and place not in trans.inPlaces:
+					trans.setInputPlaces(place)
+					place.outTransObj.append(trans)
+		
+											
 	
-	def generateSequences(self, number): # number of sequences to generate
-		sequences = []
-		for i in range(number):
+	def displayTransitions(self):
+		print("Transitions")
+		for trans in self.transitions:
+			trans.display()
+		
+	def displayPlaces(self):
+		print("Places")
+		for place in self.places:
+			place.display()
 			
-			sequence = []
-			places = []
-			for pi in range(len(self.places)):
-				if self.places[pi].name == "start":
-					places.append(self.places[pi])
-					
-					
-			endReached = False
 			
-			for pi in range(len(places)):
-				if places[pi].name == "end":
+	def generateTrace(self):
+		trace = ""
+		start = self.getPlaceByName("start")
+		start.setToken()
+		
+		endReached = False
+		places = [start]
+		while not endReached:
+			nextPlaces = []
+			for place in places:
+				transitions = place.getOutTransitions()	
+				random.shuffle(transitions)
+				for trans in transitions:
+					result = trans.fire()
+					if result != -1:
+						trace += result
+						nextPlaces.extend(trans.outPlaces)
+			random.shuffle(nextPlaces)
+			places = nextPlaces
+			for place in places:
+				if place.name == "end":
 					endReached = True
 				
-			
-			nbTokens = 0
-			while not endReached:			
-				chosenTransitions = []
-				random.shuffle(places)
-				nbTokens += len(places)
+		print(trace)
 		
-				for pi in range(len(places)):
-					transitions = places[pi].outTrans
-					
-					if nbTokens > 0 and len(transitions) > 0 and not endReached:
-						if len(transitions) > 1:
-							event = np.random.choice(transitions)
-							while nbTokens < self.inputPlaces[event]:
-								event = np.random.choice(transitions)
-							
-							if event not in chosenTransitions:
-								chosenTransitions.append(event)
-								nbTokens -= self.inputPlaces[event]
-								if event in self.endTransitions:
-									endReached = True 
-						else:
-							if nbTokens >=  self.inputPlaces[transitions[0]] and transitions[0] not in chosenTransitions:
-								chosenTransitions.append(transitions[0])
-								nbTokens -= self.inputPlaces[transitions[0]]
-								if transitions[0] in self.endTransitions:
-									endReached = True
-								
-				sequence.extend(chosenTransitions)
-				#~ print(nbTokens)
-				#~ print(sequence)
-				#~ print(endReached)
-							
-				places = []
-				for trans in chosenTransitions:
-					places.extend(self.getPlacesWithInputTrans(trans))
-				
-				#~ for pi in range(len(places)):
-					#~ if places[pi].name == "end" and loopCredit == 0:
-						#~ endReached = True
-					#~ elif places[pi].name == "end" and loopCredit > 0:
-						#~ loopCredit -= 1
-			
-			sequences.append(sequence)
-			
-		return sequences	
-	
-	def getPlacesWithInputTrans(self, inTrans):
-		result = set()
-		for place in self.places:
-			if inTrans in place.inTrans:
-				result.add(place)
-				
-		return list(result) 
-	
-	
-			
 			
 class Place:
-	def __init__(self, name, inTrans, outTrans):
+	def __init__(self, name, arcs):
+		self.name = name # name of the place
+		# two lists of input transitions and output transitions NAMES
+		self.inTrans = []
+		self.outTrans = []
+		
+		self.inTransObj = [] # ACTUAL OBJECTS
+		self.outTransObj = []
+		
+		self.token = False
+		
+		self.setInAndOutTransitions(arcs)
+		
+	def setInAndOutTransitions(self, arcs):
+		for i in range(len(arcs)):
+			if self.name == arcs[i][0]:
+				self.outTrans.extend(arcs[i][1])
+			else:
+				self.inTrans.extend(arcs[i][0])
+		
+	def isStartPlace(self):
+		return self.name == "start"
+		
+	def isEndPlace(self):
+		return self.name == "end"
+		
+	def setToken(self):
+		self.token = not self.token
+	
+	def hasToken(self):
+		return self.token
+	
+	def display(self):
+		print(self.inTrans, self.name, self.outTrans) 
+		
+	def getOutTransitions(self):
+		return self.outTransObj
+		
+class Transition:
+	def __init__(self, name):
 		self.name = name # name of the place
 		# two lists of input transitions and output transitions
-		self.inTrans = inTrans
-		self.outTrans = outTrans
-	
-
-	
-
+		
+		self.inPlaces = [] # ACTUAL OBJECTS
+		self.outPlaces = []
+		
+	def	setInputPlaces(self, place):
+		self.inPlaces.append(place)
+		
+	def setOutputPlaces(self, place):
+		self.outPlaces.append(place)
+		
+	def display(self):
+		for p in self.inPlaces:
+			print("("+p.name+")")
+		print(self.name)
+		for p in self.outPlaces:
+			print("("+p.name+")")
+		
+	def fire(self):
+		canFire = True
+		for place in self.inPlaces:
+			if not place.hasToken():
+				canFire = False
+		if canFire:
+			for place in self.inPlaces:
+				place.setToken()
+			for place in self.outPlaces:
+				place.setToken()
+			return self.name
+		return -1 
+		
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print("This program needs a .dot petri-net spec file (used by graphviz as well) as parameter")
 		exit()
 	else:
 		pn = PetriNet(sys.argv[1])
+		pn.generateTrace()
 		
-		sequences = pn.generateSequences(20)
-		print("Sequences:")
-		print(sequences)
